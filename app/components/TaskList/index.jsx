e2839ce909613ca3cc9ac97b896a614f770fcc83
Saruar <saruar.ryskaliyev@nu.edu.kz>
2024-06-06 11:35:30 +0500

    Final

--- app/components/TaskList/index.jsx
+++ app/components/TaskList/index.jsx
@@ -1,11 +1,29 @@
 import React from 'react';
 import TaskItem from '../TaskItem';
 
-const TaskList = () => {
-  // Render TaskItems using TaskItem component
-  // Filter tasks by status here
+const TaskList = ({ tasks, onToggle, onDelete, uncompletedCount, filter, setFilter, clearCompletedTasks }) => {
   return (
-    <></>
+    <div className="bg-gray-800 rounded p-4">
+      <ul>
+        {tasks.map(task => (
+          <TaskItem key={task.id} task={task} onToggle={onToggle} onDelete={onDelete} />
+        ))}
+      </ul>
+      <div className="mt-4 flex justify-between items-center text-sm text-gray-400">
+        <span>{uncompletedCount} items left</span>
+        <div>
+          <button onClick={() => setFilter('all')} className={`mr-2 ${filter === 'all' ? 'text-white' : ''}`}>All</button>
+          <button onClick={() => setFilter('active')} className={`mr-2 ${filter === 'active' ? 'text-white' : ''}`}>Active</button>
+          <button onClick={() => setFilter('completed')} className={`${filter === 'completed' ? 'text-white' : ''}`}>Completed</button>
+        </div>
+        <button
+          onClick={clearCompletedTasks}
+          className="text-gray-400 hover:text-white"
+        >
+          Clear Completed
+        </button>
+      </div>
+    </div>
   );
 };
 
--- app/page.js
+++ app/page.js
@@ -1,11 +1,23 @@
 'use client'
-import Image from 'next/image';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import TaskList from './components/TaskList';
+
+/* eslint-disable react/no-unescaped-entities */
 
 export default function Home() {
-  const [tasks, setTasks] = useState([]);
+  const [tasks, setTasks] = useState(() => {
+    return JSON.parse(localStorage.getItem('tasks')) || [];
+  });
+
   const [filter, setFilter] = useState('all');
   const [newTaskText, setNewTaskText] = useState('');
+
+
+
+  useEffect(() => {
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+    console.log(localStorage.getItem('tasks'));
+  }, [tasks]);
 
   const handleAddTask = () => {
     if (newTaskText.trim()) {
@@ -55,47 +67,15 @@ export default function Home() {
           Add Task
         </button>
       </div>
-      <div className="bg-gray-800 rounded p-4">
-        <ul>
-          {filteredTasks.map(task => (
-            <li key={task.id} className="flex justify-between items-center p-2 bg-gray-900 rounded mb-2">
-              <div className="flex items-center">
-                <button 
-                  className="w-6 h-6 my-auto mr-6"
-                  onClick={() => handleToggleTask(task.id)} 
-                >
-                  <Image
-                    src={task.completed ? "/images/circle-checked.svg" : "/images/circle.svg"}
-                    alt="Task status"
-                    width={30}
-                    height={30}
-                  />
-                </button>
-                <span className={`ml-2 ${task.completed ? 'line-through text-gray-500' : 'text-white'}`}>{task.text}</span>
-              </div>
-              <button onClick={() => handleDeleteTask(task.id)} className="text-gray-400 hover:text-white">
-                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
-                </svg>
-              </button>
-            </li>
-          ))}
-        </ul>
-        <div className="mt-4 flex justify-between items-center text-sm text-gray-400">
-          <span>{uncompletedCount} items left</span>
-          <div>
-            <button onClick={() => setFilter('all')} className={`mr-2 ${filter === 'all' ? 'text-white' : ''}`}>All</button>
-            <button onClick={() => setFilter('active')} className={`mr-2 ${filter === 'active' ? 'text-white' : ''}`}>Active</button>
-            <button onClick={() => setFilter('completed')} className={`${filter === 'completed' ? 'text-white' : ''}`}>Completed</button>
-          </div>
-          <button
-            onClick={() => setTasks(tasks.filter(task => task.completed === false))}
-            className="text-gray-400 hover:text-white"
-          >
-            Clear Completed
-          </button>
-        </div>
-      </div>
+      <TaskList
+        tasks={filteredTasks}
+        onToggle={handleToggleTask}
+        onDelete={handleDeleteTask}
+        uncompletedCount={uncompletedCount}
+        filter={filter}
+        setFilter={setFilter}
+        clearCompletedTasks={() => setTasks(tasks.filter(task => !task.completed))}
+      />
     </div>
   );
 }
